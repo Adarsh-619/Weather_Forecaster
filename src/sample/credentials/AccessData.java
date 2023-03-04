924f92a41cfac1e29bfb1fd9aa45f5f9dc63f3de
Adarsh Patra <patra.adarsh9@gmail.com>
2023-03-04 23:50:35 +0530

    Used my api key of OpenWeather, Don't misuse anyone please

--- src/sample/credentials/AccessData.java
+++ src/sample/credentials/AccessData.java
@@ -1,7 +1,7 @@
 package sample.credentials;
 
 public class AccessData {
-    private static final String API_KEY = "<Your API Key Here>";
+    private static final String API_KEY = "b9d82d47340ecf4456de5ef1ccb929ef";
     public static String getAPIKey(){
         return API_KEY;
     }
